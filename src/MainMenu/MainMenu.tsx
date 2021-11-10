2d3a05519b23591fa5aa2bab6e4c7fc38ca9ec5f
Matthais Harzer <matthias.harzer03@gmail.com>
2021-11-10 10:50:17 +0100

    + Added WSAddress Cracker -> nimmt als input eine (Teil-)IP (z.B. 192.168, 192.168.0). + Curstom IP input in MainMenu wo (teil-)IPs eingegeben werden und im localStorage gespeichert werden + server connect

--- src/MainMenu/MainMenu.tsx
+++ src/MainMenu/MainMenu.tsx
@@ -13,14 +13,16 @@ export class MainMenu extends Component<any, any> {
             this.setState({ logged_in: true });
             this.setUserName(this.state.name);
         });
-        network.onReceive("SERVER_CONNECT", ()=>{
-            this.setState({server_connected: true});
-            if (this.ellipsesTimer) {
-                clearInterval(this.ellipsesTimer);
-            }
-        })
+        network.onReceive("SERVER_CONNECT", () => {
+            this.setState({ server_connected: true });
 
-        let dots = ""
+        });
+        network.onReceive("SERVER_DISCONNECT", () => {
+            this.setState({ server_connected: false });
+
+        });
+
+        let dots = "";
         this.ellipsesTimer = setInterval(() => {
             dots += ".";
             if (dots.length > 4) {
@@ -29,6 +31,7 @@ export class MainMenu extends Component<any, any> {
             this.setState({ ellipses_render: dots });
         }, 500);
     }
+
     componentWillUnmount() {
         if (this.ellipsesTimer) {
             clearInterval(this.ellipsesTimer);
@@ -49,20 +52,25 @@ export class MainMenu extends Component<any, any> {
 
     };
 
-    createMatch = async() =>{
+    createMatch = async () => {
         const matchID = await network.createGame();
 
-        if(matchID){
+        if (matchID) {
             this.props.onGameConnected(matchID);
         }
-    }
-    joinMatch = async() =>{
-        const state = await network.joinGame(this.state?.inputMatchID || "")
-        if(state!=="SUCCESS"){
-            alert("Error: " + state)
-        }else{
+    };
+    joinMatch = async () => {
+        const state = await network.joinGame(this.state?.inputMatchID || "");
+        if (state !== "SUCCESS") {
+            alert("Error: " + state);
+        } else {
             this.props.onGameConnected(this.state?.inputMatchID);
         }
+    };
+
+    useCustomIPAddress = (ip: string) =>{
+        console.log("Using custom IP: " + ip);
+        network.findServerByIP(ip);
     }
 
 
@@ -70,36 +78,47 @@ export class MainMenu extends Component<any, any> {
         return (
             <>
                 <div className={"bg"}>
+                    <div className={"ip-address-template-input"}>
+                        <input type={"text"} defaultValue={localStorage.getItem("server_ip")||""} onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                this.useCustomIPAddress(e.currentTarget.value);
+                            }}
+                        }
+                               placeholder={"Custom IP"}
+                        />
+                    </div>
                     {/*<h1 className={"bg-header"}>{Array(100).fill("Schiffe Versenken ").map(r=>(<span style={{fontSize: Math.random()*120 + 15}}>{r}</span>))}</h1>*/}
                     <div className={"menu"}>
                         {this.state?.server_connected ?
                             <>
-                        <div className={"login combo"}>
-                            <input type={"text"} onKeyDown={(e) => {
-                                if (e.key === "Enter") {
-                                    this.login();
-                                }
-                            }} value={this.state?.name || ""} onChange={(e) => this.setState({ name: e.target.value })}
-                                   placeholder={"Name"}
-                            />
-                            <button onClick={this.login}>Set Name</button>
-                        </div>
-                        <div className={(this.state?.logged_in ? "" : "hidden") + " game-select"}>
-                            <h3>Host or join a game</h3>
-                            <div className={"host"}>
-                                <button onClick={this.createMatch}>Host Game</button>
-                            </div>
-                            <div className={"join combo"}>
-                                <input placeholder={"Match ID"} type={"text"} value={this.state?.inputMatchID} onChange={(e)=>this.setState({inputMatchID: e.target.value.toUpperCase()})} />
-                                <button onClick={this.joinMatch}>Join</button>
-                            </div>
-                        </div>
+                                <div className={"login combo"}>
+                                    <input type={"text"} onKeyDown={(e) => {
+                                        if (e.key === "Enter") {
+                                            this.login();
+                                        }
+                                    }} value={this.state?.name || ""}
+                                           onChange={(e) => this.setState({ name: e.target.value })}
+                                           placeholder={"Name"}
+                                    />
+                                    <button onClick={this.login}>Set Name</button>
+                                </div>
+                                <div className={(this.state?.logged_in ? "" : "hidden") + " game-select"}>
+                                    <h3>Host or join a game</h3>
+                                    <div className={"host"}>
+                                        <button onClick={this.createMatch}>Host Game</button>
+                                    </div>
+                                    <div className={"join combo"}>
+                                        <input placeholder={"Match ID"} type={"text"} value={this.state?.inputMatchID}
+                                               onChange={(e) => this.setState({ inputMatchID: e.target.value.toUpperCase() })} />
+                                        <button onClick={this.joinMatch}>Join</button>
+                                    </div>
+                                </div>
                             </> : <>
                                 <div className={"loading"}>
-                                    <div className="loader"/>
+                                    <div className="loader" />
                                     <span>Connecting to server{this.state?.ellipses_render}</span>
                                 </div>
-                            </> }
+                            </>}
                     </div>
                 </div>
             </>
