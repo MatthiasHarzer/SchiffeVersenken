68928e8849752bbc68c35ab0c56985781ef47a13
Matthais Harzer <matthias.harzer03@gmail.com>
2021-11-17 13:49:02 +0100

    Improved Chat

--- src/chat/Chat.tsx
+++ src/chat/Chat.tsx
@@ -2,7 +2,8 @@ import "./Chat.css"
 import { Component } from "react";
 import { network } from "../network";
 import { game } from "../game";
-
+import { formatDate } from "../util";
+import send from "../assets/imgs/send.png";
 const scrollTo = (ref: any) => {
 
     if (ref ) {
@@ -10,15 +11,29 @@ const scrollTo = (ref: any) => {
     }
 }
 
+type IChatState = {
+    messages: any[];
+    sendMessage: string;
+    unread: number;
+    open: boolean;
+}
 
+export class Chat extends Component<any, IChatState>{
+    constructor(props: IChatState) {
+        super(props);
 
-export class Chat extends Component<any, any>{
-
+        this.state = {
+            messages: [],
+            sendMessage: "",
+            unread: 0,
+            open: false
+        }
+    }
 
     componentDidMount() {
         network.onReceive("CHAT", data=>{
             data?.message && this.setState({
-                messages: [...this.state?.messages || [], {text: data?.message, playerNr: data?.playerNr}]
+                messages: [...this.state.messages || [], {text: data?.message, playerNr: data?.playerNr}]
             })
             if(!this.state?.open){
                 this.setState({
@@ -40,6 +55,16 @@ export class Chat extends Component<any, any>{
         })
     }
 
+    sendMessage = ()=>{
+        if(this.state?.sendMessage){
+
+            network.send("CHAT", { message: this.state?.sendMessage })
+        }
+        this.setState({
+            sendMessage: ""
+        })
+}
+
     render() {
         return (<div className={"chat "  + (this.state?.open ? "chat-open" : "")}>
             <div className={"header"}>
@@ -50,19 +75,31 @@ export class Chat extends Component<any, any>{
             <div className="chat-messages">
                 <div className={"top-fade"} > <div/></div>
                 {this.state?.messages && this.state?.messages.map((message: any, index: number) => {
-                    console.log(message, game?.playerNr)
                     return (<div key={index} className={"chat-message " + ((!isNaN(Number(message?.playerNr)) && message.playerNr === game?.playerNr) ? "me" : "")} ref={scrollTo}>
                         <span className={"chat-message-player"}>{(game.players && game.players[message.playerNr]) || "unknown"}</span>
                         <span className={"chat-message-text"}>{message.text}</span>
+                        <span className={"chat-message-time"}>{formatDate(new Date(), "#hhhh#:#mm#")}</span>
                     </div>)})}
             </div>
             <div className="chat-input">
-                <input type="text" placeholder="Type a message..." onKeyPress={(e) => {
+                <div className={"input-wrapper"}>
+
+                <input type="text" placeholder="Type a message..." value={this.state.sendMessage} onChange={e=>{
+                    this.setState({
+                        sendMessage: e.currentTarget.value
+                    })
+                }}
+                       onKeyPress={(e) => {
+
                     if (e.key === "Enter") {
-                        network.send("CHAT", { message: e.currentTarget.value })
-                        e.currentTarget.value = ""
+                        this.sendMessage();
+                        // e.currentTarget.value = ""
                     }
                 }} />
+                <button className={"clear-btn"} onClick={this.sendMessage}>
+                    <img src={send} alt={">>>"}/>
+                </button>
+                </div>
             </div>
         </div>);
     }
--- src/util.tsx
+++ src/util.tsx
@@ -8,6 +8,41 @@ export const random_string = () => {
     return (Math.random() + 1).toString(36).substring(7);
 };
 
+/*
+ * @param date: Date object to convert to date string
+ * @param format: Date format to use
+ *      -> https://stackoverflow.com/a/4673990/11664234
+ * @returns: Date string
+ */
+export const formatDate = (date: Date, formatString: string) =>{
+    if(date.getTime()===0){
+        return "";
+    }
+
+    var YYYY, YY, MMMM, MMM, MM, M, DDDD, DDD, DD, D, hhhh, hhh, hh, h, mm, m, ss, s, ampm, AMPM, dMod, th, ms;
+    YY = ((YYYY = date.getFullYear()) + "").slice(-2);
+    MM = (M = date.getMonth() + 1) < 10 ? ('0' + M) : M;
+    MMM = (MMMM = ["Jan.", "Feb.", "März", "April", "Mai", "Juni", "Juli", "Aug.", "Sept.", "Okt.", "Nov.", "Dez."][M - 1]).substring(0, 3);
+    DD = (D = date.getDate()) < 10 ? ('0' + D) : D;
+    DDD = (DDDD = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"][date.getDay()]).substring(0, 3);
+    th = (D >= 10 && D <= 20) ? 'th' : ((dMod = D % 10) === 1) ? 'st' : (dMod === 2) ? 'nd' : (dMod === 3) ? 'rd' : 'th';
+    // @ts-ignore
+    formatString = formatString.replace("#YYYY#", YYYY).replace("#YY#", YY).replace("#MMMM#", MMMM).replace("#MMM#", MMM).replace("#MM#", MM).replace("#M#", M).replace("#DDDD#", DDDD).replace("#DDD#", DDD).replace("#DD#", DD).replace("#D#", D).replace("#th#", th);
+    h = (hhh = date.getHours());
+    if (h === 0) h = 24;
+    if (h > 12) h -= 12;
+    hh = h < 10 ? ('0' + h) : h;
+    hhhh = hhh < 10 ? ('0' + hhh) : hhh;
+    AMPM = (ampm = hhh < 12 ? 'am' : 'pm').toUpperCase();
+    mm = (m = date.getMinutes()) < 10 ? ('0' + m) : m;
+    ss = (s = date.getSeconds()) < 10 ? ('0' + s) : s;
+    ms = date.getMilliseconds();
+
+
+
+    // @ts-ignore
+    return formatString.replace("#hhhh#", hhhh).replace("#hhh#", hhh).replace("#hh#", hh).replace("#h#", h).replace("#mm#", mm).replace("#m#", m).replace("#ss#", ss).replace("#s#", s).replace("#ampm#", ampm).replace("#AMPM#", AMPM).replace("#ms#", ms);
+}
 
 export default function isDev(): boolean {
     return "_self" in React.createElement("div");
